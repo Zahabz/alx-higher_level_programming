93f70453984b55c16ddfac1157338833fabeda06
Zahabz <herbert.bett54@gmail.com>
2022-12-02 06:52:19 +0300

    Fixing indent issue

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,11 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 
- if __name__ == '__main__':
-     """Importing hidden_4.pyc"""
-     import hidden_4
+if __name__ == '__main__':
+    import hidden_4
+    names = dir(hidden_4)
 
-     names = dir(hidden_4)
-
-     for name in names:
-         if name.startswith("__"):
-             continue
-         else:
-             print(name)
+    for name in names:
+        if name.startswith('__'):
+            continue
+        else:
+            print(name)
