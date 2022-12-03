59326426ce07c31427d5f2c798bd42f8d48ee3f1
Zahabz <herbert.bett54@gmail.com>
2022-12-03 11:48:37 +0300

    Removing test case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,13 +2,7 @@
 
 def multiple_returns(sentence):
     if sentence == '':
-        return None
-    
-    mul_tuple = len(sentence), sentence[0]
-
-    return mul_tuple
-
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+        sentence[0] = None
+    else:
+        mul_tuple = len(sentence), sentence[0]
+        return mul_tuple
