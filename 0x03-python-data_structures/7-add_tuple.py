8c8551abd902485b742b72ecfc7e03d7a4ef74af
Zahabz <herbert.bett54@gmail.com>
2022-12-03 10:56:42 +0300

    Solving integer issue

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,8 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) < 2:
         tuple_b = tuple_b + (0,)
     
-    element_1 = tuple_a[0] + tuple_b[0]
-    element_2 = tuple_a[1] + tuple_b[1]
+    element_1 = int(tuple_a[0]) + int(tuple_b[0])
+    element_2 = int(tuple_a[1]) + int(tuple_b[1])
     new_tuple = (element_1, element_2)
     
     return new_tuple
