9134f1899706cf72956e42d783a9e3b63e6988f9
Zahabz <herbert.bett54@gmail.com>
2022-12-03 10:58:43 +0300

    Accounting for empty tuple issue solved

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,9 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2 :
-        tuple_a = tuple_a + (0,)
+        tuple_a = tuple_a + (0,0)
     elif len(tuple_b) < 2:
-        tuple_b = tuple_b + (0,)
+        tuple_b = tuple_b + (0,0)
     
     element_1 = int(tuple_a[0]) + int(tuple_b[0])
     element_2 = int(tuple_a[1]) + int(tuple_b[1])
