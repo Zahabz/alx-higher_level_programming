0cd23bc2cba1bdfb7f2771bad8db19bb07e6078c
Zahabz <herbert.bett54@gmail.com>
2022-12-07 14:12:14 +0300

    Quick file fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary.keys():
+    if key not in a_dictionary.keys():
         a_dictionary[key] = value
     else:
         a_dictionary[key] = value
 
-    for key, value in a_dictionary.items():
-        print('{}: {}'.format(key, value))
+    return a_dictionary
