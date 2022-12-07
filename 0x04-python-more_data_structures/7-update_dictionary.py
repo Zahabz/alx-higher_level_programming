158248c60997fdc5a3ec3df8710edef4b7657979
Zahabz <herbert.bett54@gmail.com>
2022-12-07 13:58:04 +0300

    Mistakes corrected

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -8,10 +8,3 @@ def update_dictionary(a_dictionary, key, value):
 
     for key, value in a_dictionary.items():
         print('{}: {}'.format(key, value))
-
-
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-new_dict
-
-
