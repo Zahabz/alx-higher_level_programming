9cd8e44f22ac9ea68a1be5822874904873155141
Zahabz <herbert.bett54@gmail.com>
2022-12-07 14:29:10 +0300

    Code optimized

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -7,7 +7,3 @@ def simple_delete(a_dictionary, key=""):
         del a_dictionary[key]
     
     return a_dictionary
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
-new_dict = simple_delete(a_dictionary, 'tra')
-print(new_dict)
