d73f6f7f245958410f6d4f38cfa674c8af0376a4
Zahabz <herbert.bett54@gmail.com>
2022-12-07 14:28:09 +0300

    Fixed ommission

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
     if key not in a_dictionary.keys():
@@ -7,3 +7,7 @@ def simple_delete(a_dictionary, key=""):
         del a_dictionary[key]
     
     return a_dictionary
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'tra')
+print(new_dict)
