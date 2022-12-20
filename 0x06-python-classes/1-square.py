6be882df5ad622db0e2387b68c392d5900001a7a
Zahabz <herbert.bett54@gmail.com>
2022-12-20 14:39:38 +0300

    File upload

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Creation/Defintion of the Square class"""
+
+class Square:
+    """
+    Class to represent a Square
+
+    Attributes:
+        size(int): Represents the square size (Private instance attribute). 
+    """
+    def __init__(self, size):
+        """
+        Args:
+            size (int): Representation of the square size 
+        """
+        self.__size = size
+
