9a7ee8832500434b9fec230585c8d6d071656212
Zahabz <herbert.bett54@gmail.com>
2022-12-21 12:35:06 +0300

    Correction

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -125,12 +125,12 @@ class Square:
          A magic method that prints a string representation of the Square object/instance
 
         """
-        for y in range(self.__position[1]):
-            print("")
 
         if self.__size == 0:
             print("")
         else:
+            for y in range(self.__position[1]):
+                print("")
             for i in range(self.__size):
                 for x in range(self.__position[0]):
                     print(' ', end='')
