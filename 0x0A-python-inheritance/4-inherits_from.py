34d2271ec23995f20eb78f7cc48fd6250467e729
Zahabz <herbert.bett54@gmail.com>
2023-01-09 19:29:26 +0300

    Iherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.
+    
+    Parameters:
+    -----------
+        - obj(any): Object to be tested
+        - a_class(object): class object to test with
+
+    Returns:
+    --------
+        Returns `True` or `False`
+
+    """
+    if issubclass(type(obj), a_class):
+        return True
+    return False
+
