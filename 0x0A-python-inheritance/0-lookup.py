9f66237a8ddc153cef38a4e54857fc8654635171
Zahabz <herbert.bett54@gmail.com>
2023-01-09 18:14:15 +0300

    Fixing permissions

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,16 +4,3 @@ def lookup(obj):
     obj_list = list(dir(obj))
 
     return obj_list
-
-
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
