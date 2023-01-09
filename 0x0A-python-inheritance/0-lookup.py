dcbdd4e068942f91c81f0d43b32ce9e906f47070
Zahabz <herbert.bett54@gmail.com>
2023-01-09 18:24:09 +0300

    Documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
+""" Module containing `def lookup(obj):` function."""
 def lookup(obj):
+    """
+    Collects all attributes and methods in a class object into a list
+
+    Parameters:
+    -----------
+        - obj: A class object
+
+    Returns:
+    --------
+        Returns a list object of the attributes and methods available for the object 
+    """
     obj_list = list(dir(obj))
 
     return obj_list
