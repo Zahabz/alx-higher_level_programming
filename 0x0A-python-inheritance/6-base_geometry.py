105b63dfb06d86a125072a0e10e20c6c6b74e5ab
Zahabz <herbert.bett54@gmail.com>
2023-01-09 19:50:32 +0300

    Fixing errors

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -18,10 +18,3 @@ class BaseGeometry:
             Raises a `NotImplementedError` exception
         """
         raise Exception('area() is not implemented')
-
-bg = BaseGeometry()
-
-try:
-    print(bg.area())
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
